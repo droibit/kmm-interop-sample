81fd42f206e2dce05f0367d398835bf08cc85ce8
Shinya Kumagai <roomful.rooms@gmail.com>
2021-01-07 18:53:03 +0900

    Remove App/SceneDelegate

---
+++ ios/ios/SampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  SampleApp.swift
+//  ios
+//
+//  Created by Shinya Kumagai on 2021/01/07.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
